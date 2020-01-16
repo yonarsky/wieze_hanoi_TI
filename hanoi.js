c342d47c3f67fa1dbeab38eb8f5cb076ac4b12b1
Adrian <lxge99@gmail.com>
2020-01-16 20:40:21 +0100

    async

--- hanoi.js
+++ hanoi.js
@@ -1,17 +1,35 @@
 /*eslint-env browser*/
 var button = document.getElementById("buttonId");
-function hanoiTower(x, A, B, C) {
+var asd="";
+async function hanoiTower(x, A, B, C) {
     if (x > 0 ) {
         hanoiTower(x - 1, A, C, B);
-        document.getElementById('list').innerHTML += '<li class="list-group-item">Krążek nr ' + x + ' z <b>' + A + '</b> na <b>' + C + '</b></li>';
+        asd+='<li class="list-group-item">Krążek nr ' + x + ' z <b>' + A + '</b> na <b>' + C + '</b></li>';
         hanoiTower(x - 1, B, A, C);
     }
 }
-button.onclick = function() {
-    document.getElementById("list").innerHTML = "";
+
+
+//button.onclick = function() {
+//    document.getElementById('list').textContent = "";
+//    console.log(document.getElementById('list').textContent);
+//    var n = parseInt(document.getElementById('number-input').value, 10);
+//    var k = Math.pow(2,n)-1;
+//    document.getElementById('list').innerHTML += '<button type="button" class="btn btn-primary" id="num"> Liczba kroków <span class="badge badge-light">' + k + '</span>'
+//    hanoiTower(n,"A","B","C");
+//};
+
+
+button.addEventListener('click',()=>{
+    asd="";
+    document.getElementById('list').innerHTML = "";
+    console.log(document.getElementById('list').textContent);
     var n = parseInt(document.getElementById('number-input').value, 10);
     var k = Math.pow(2,n)-1;
-    document.getElementById('list').innerHTML += '<button type="button" class="btn btn-primary" id="num"> Liczba kroków <span class="badge badge-light">' + k + '</span>'
-    hanoiTower(n,"A","B","C");
-};
+    document.getElementById('list').innerHTML += '<button type="button" class="btn btn-primary" id="num"> Liczba kroków <span class="badge badge-light">' + k + '</span>';
+    hanoiTower(n,"A","B","C").then(()=>{
+        document.getElementById('list').innerHTML+=asd;
+    console.log("zakonczono");
+                                       })
 
+})
