cd0f9c71f2dd4a6d3ed1c8f8ff8d16827f73325e
Adrian <lxge99@gmail.com>
2020-01-20 10:22:31 +0100

    zwiekszenie zakresu

--- hanoi.js
+++ hanoi.js
@@ -1,35 +1,21 @@
 /*eslint-env browser*/
 var button = document.getElementById("buttonId");
-var asd="";
+var list = "";
 async function hanoiTower(x, A, B, C) {
-    if (x > 0 ) {
+    if (x > 0) {
         hanoiTower(x - 1, A, C, B);
-        asd+='<li class="list-group-item">Krążek nr ' + x + ' z <b>' + A + '</b> na <b>' + C + '</b></li>';
+        list += '<li class="list-group-item">Krążek nr ' + x + ' z <b>' + A + '</b> na <b>' + C + '</b></li>';
         hanoiTower(x - 1, B, A, C);
     }
 }
 
-
-//button.onclick = function() {
-//    document.getElementById('list').textContent = "";
-//    console.log(document.getElementById('list').textContent);
-//    var n = parseInt(document.getElementById('number-input').value, 10);
-//    var k = Math.pow(2,n)-1;
-//    document.getElementById('list').innerHTML += '<button type="button" class="btn btn-primary" id="num"> Liczba kroków <span class="badge badge-light">' + k + '</span>'
-//    hanoiTower(n,"A","B","C");
-//};
-
-
 button.addEventListener('click',()=>{
-    asd="";
+    list = "";
     document.getElementById('list').innerHTML = "";
-    console.log(document.getElementById('list').textContent);
     var n = parseInt(document.getElementById('number-input').value, 10);
     var k = Math.pow(2,n)-1;
     document.getElementById('list').innerHTML += '<button type="button" class="btn btn-primary" id="num"> Liczba kroków <span class="badge badge-light">' + k + '</span>';
     hanoiTower(n,"A","B","C").then(()=>{
-        document.getElementById('list').innerHTML+=asd;
-    console.log("zakonczono");
-                                       })
-
+        document.getElementById('list').innerHTML += list;
+    })
 })
